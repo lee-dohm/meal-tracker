85813a4e3934ab7c62755fb951797905efc5b52d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-21 15:53:46 -0700

    Extract help command into its own module

--- lib/meal_tracker/cli.ex
+++ lib/meal_tracker/cli.ex
@@ -3,17 +3,17 @@ defmodule MealTracker.CLI do
   Handles the command-line interface of the meal tracker application.
   """
 
-  alias MealTracker.Commands.{Add, List, Status, Version}
+  alias MealTracker.Commands.{Add, Help, List, Status, Version}
 
   def main(argv \\ []) do
     case argv do
-      [] -> print_help()
+      [] -> Help.run([])
       [command | options] -> handle_command(command, options)
     end
   end
 
   defp handle_command("add", options), do: Add.run(options)
-  defp handle_command("help", _options), do: print_help()
+  defp handle_command("help", options), do: Help.run(options)
   defp handle_command("list", options), do: List.run(options)
   defp handle_command("status", options), do: Status.run(options)
   defp handle_command("version", options), do: Version.run(options)
@@ -21,21 +21,6 @@ defmodule MealTracker.CLI do
   defp handle_command(command, _options) do
     IO.puts("Unknown command: #{command}\n\n")
 
-    print_help()
-  end
-
-  defp print_help do
-    Version.run([])
-
-    IO.puts("""
-
-    These are the common Meal Tracker commands:
-
-    add       Add a log entry
-    help      Print this help information
-    list      List the daily logs
-    status    Print today's log
-    version   Print Meal Tracker version information
-    """)
+    Help.run([])
   end
 end
---
+++ lib/meal_tracker/commands/help.ex
@@ -0,0 +1,15 @@
+defmodule MealTracker.Commands.Help do
+  def run(_options) do
+    IO.puts("""
+    #{MealTracker.version_text()}
+
+    These are the common Meal Tracker commands:
+
+    add       Add a log entry
+    help      Print this help information
+    list      List the daily logs
+    status    Print today's log
+    version   Print Meal Tracker version information
+    """)
+  end
+end
