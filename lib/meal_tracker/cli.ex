524a159cb1c5264c92d2b4fc0dd379cae80db6b3
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-21 13:55:32 -0700

    Add add command

--- lib/meal_tracker/cli.ex
+++ lib/meal_tracker/cli.ex
@@ -3,13 +3,24 @@ defmodule MealTracker.CLI do
   Handles the command-line interface of the meal tracker application.
   """
 
-  alias MealTracker.Config
+  alias MealTracker.{Config, FoodItem, Log}
 
   def main(argv \\ []) do
     case argv do
       [] -> print_help()
       [command | options] -> handle_command(command, options)
     end
+  end
+
+  defp handle_command("add", options) do
+    entry =
+      options
+      |> Enum.join(" ")
+      |> FoodItem.parse()
+
+    read_or_create_today()
+    |> Log.add_entry(entry)
+    |> write_today()
   end
 
   defp handle_command("help", _options) do
@@ -40,9 +51,25 @@ defmodule MealTracker.CLI do
     handle_command("help", [])
   end
 
+  defp read_or_create_today do
+    path = today_path()
+
+    if File.exists?(path) do
+      {:ok, log} = Log.read(path)
+
+      log
+    else
+      Log.new()
+    end
+  end
+
   defp today_path do
     today = NaiveDateTime.utc_now() |> NaiveDateTime.to_date()
 
     Path.join(meal_tracker_path(), "#{Date.to_iso8601(today)}.md")
+  end
+
+  defp write_today(log) do
+    Log.write(log, today_path())
   end
 end
